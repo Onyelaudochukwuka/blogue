2bcc9f9f0e5d636dca7bcbfe887440fc0fc3ba7c
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2022-06-10 17:36:35 +0100

    changes

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -18,11 +18,29 @@ else{
   
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
-      <h3r classNamer="text-xl mb-8 font-semibold border-b pb-4">
+      <h3 classNamer="text-xl mb-8 font-semibold border-b pb-4">
       {slug ? 'Related Posts' : 'Recent Posts'}
       </h3>
       {relatedPosts.map((post)=>(
-        <div></div>
+        <div key={post.title} className="flex items-center w-full mb-4">
+            <div className="w-16 flex-none">
+              <img 
+              alt={post.title}
+              height="60px"
+              width="60px"
+              className="align-middle rounded-full"
+              src={post.featuredImage.url}
+              />
+            </div>
+            <div className="flex-grow ml-4">
+          <p className="text-gray-500 text-xs">
+            {moment(post.createdAt).format('MM DD, YYYY')}
+          </p>
+          <Link href={`post/${post.slug}`} className="text-md">
+            {post.title}
+          </Link>
+            </div>
+        </div>
       ))}
     </div>
   )
