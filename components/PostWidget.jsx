60e2ed976d6632a83dbf735f1abf995a1deb7f23
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2022-06-10 16:41:49 +0100

    UPDATE THE RECENT POSTS SCREN

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -18,7 +18,7 @@ else{
   
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
-      <h3r classNamer="text-xl mb-8 font-semibold border-b PB-4">
+      <h3r classNamer="text-xl mb-8 font-semibold border-b pb-4">
       {slug ? 'Related Posts' : 'Recent Posts'}
       </h3>
     </div>
