01baec7b0c65e4776dfde771291f2bcbec7fa57d
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2022-06-12 18:30:41 +0100

    added a query for the details

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -6,17 +6,19 @@ import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm } fr
 const PostDetails = () => {
   return (
     <div className="container mx-auto px-10 mb-8">
-        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-            <div classsNAme="col-span-1 lg:col-span-8">
-            <PostDetail />
-            <Author />
-            <CommentsForm />
-            <Comments />
-            </div>
-            <div className="relative lg:sticky top-8">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+              <PostDetail />
+              <Author />
+              <CommentsForm />
+              <Comments />
+          </div>
+          <div className="lg:col-span-4 col-span-1">
+            <div className="lg:sticky relative top-8">
                 <PostWidget />
                 <Categories />
-            </div>
+              </div>
+          </div>
         </div>
     </div>
   )
