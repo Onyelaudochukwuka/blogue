db1e63336cef3d88ff10b41ce68308b7940aa35d
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2022-06-13 19:04:05 +0100

    set up queries

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -18,7 +18,7 @@ else{
   
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
-      <h3 classNamer="text-xl mb-8 font-semibold border-b pb-4">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
       {slug ? 'Related Posts' : 'Recent Posts'}
       </h3>
       {relatedPosts.map((post)=>(
