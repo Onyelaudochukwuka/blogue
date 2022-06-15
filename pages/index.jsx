2e1c562968c690753b1aa9508854f11b08bce108
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2022-06-15 11:54:29 +0100

    fixes

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { PostCard, Categories, PostWidget } from '../components';
 import { getPosts } from '../services';
+import { FeaturedPosts } from '../sections';
 const Home = ({ posts }) => {
   return (
     <div className="container mx-auto px-10 mb-8">
@@ -9,6 +10,7 @@ const Home = ({ posts }) => {
         <title>CMS BLOG</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
         {posts.map((post, index)=><PostCard post={post.node} key={index}/>)}
--- services/index.js
+++ services/index.js
@@ -149,11 +149,25 @@ export const getComments = async (slug) =>{
   const result = await request(graphqlAPI, query, { slug });
   return result.comments;
 }
-export const getFeaturdPosts = async () => {
+export const getFeaturedPosts = async () => {
   const query = gql`
   query GetCategoryPost(){
     posts(where: {featurePost: true}){
-      
+      author {
+        name 
+        photo {
+          url
+        }
+      }
+      featuredImage{
+        url
+      }
+      title
+      slug
+      createdAt
     }
-  }`
+  }
+  `;
+  const result = await request(graphqlAPI, query);
+  return result.posts;
 }
