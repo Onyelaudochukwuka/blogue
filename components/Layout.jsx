093edfe2727b891824f49f0d40c72f047ea718e2
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2022-06-15 22:03:42 +0100

    fixing changes

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Header } from './';
+import { Helmet } from "react-helmet";
 
 const Layout = ({children}) => {
   return (
-    <body className="bg-gradient-to-r from-violet-500 to-fuchsia-500 h-screen">
+    <div>
+       <Helmet> <style> {"body { backgroun-iImage: linear-gradient(-225deg, #69EACB 0%, #EACCF8 48%, #6654F1 100%);}"}</style> </Helmet> 
       <Header />  
       {children}
-    </body>
+    </div>
   )
 }
 
--- pages/settings.jsx
+++ pages/settings.jsx
@@ -1,15 +1,14 @@
 import React,{useState, useEffect} from 'react'
-const div1 = {
-  backgroundImage: "linear-gradient(-225deg, #69EACB 0%, #EACCF8 48%, #6654F1 100%)";
-}
 const settings = () => {
-  const [storage setStorage] = useEffect('');
-  //background-image: linear-gradient(-225deg, #69EACB 0%, #EACCF8 48%, #6654F1 100%);
+    const [storage, setStorage] = useState('');
+    const div1 = {
+      backgroundImage: "linear-gradient(-225deg, #69EACB 0%, #EACCF8 48%, #6654F1 100%)",
+    }
   return (
     <div className="m-auto grid md:grid-cols-4 grid-cols-1">
-        <div className="bg-gradient-to-r from-cyan-500 to-blue-500" style={div1}></div>
+        <span className="w-1/3 h-12" style={div1}></span>
     </div>
   )
 }
 
-export default settings
+export default settings;
