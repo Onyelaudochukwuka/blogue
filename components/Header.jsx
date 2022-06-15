fe6231f9ad7a5481f225ebef586ac01350105e7f
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2022-06-15 20:16:17 +0100

    fixed drop down

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <>
     <div className="container mx-auto px-10 mb-8">
             <div className="border-b w-full inline-block border-blue-400 py-8">
-                <div className="md:float-left block mr-auto">
+                <div className="md:float-left inline-block mr-auto py-auto mt-[32.5px]">
                     <Link href="/">
                         <span className="cursor-pointer font-bold text-4xl text-white">
                             Blogue
@@ -16,21 +16,22 @@ const Header = () => {
                 </div>
                 <div className="hidden md:float-left md:contents">
                 <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
-                <Image src="/../public/OIP.jfif" width="75px" height= "75px" className="rounded-full" onClick={()=>setDrop((!drop))}/>
-                { drop && <div className="absolute right-">
-                    <Link href="/profile"><span className="md:float-right mt-2 align-middle text-white ml- font-semibold cursor-pointer">
+                <Image src="/../public/OIP.jfif" width="75px" height= "75px" className="rounded-full inline-block ring-2 ring-white" onClick={()=>setDrop((!drop))}/>
+                { drop && <div className="absolute right-6 flex flex-col bg-white shadow-lg rounded-lg p-4 mb-8">
+                    <Link href="/profile"><span className="mb-8 font-black border-b pb-4 md:float-right mt-2 align-middle text-black cursor-pointer hover:text-gray-400 z-10">
                     My Profile
                 </span></Link>
-                    <Link href="/settings"><span className="md:float-right mt-2 align-middle text-white ml- font-semibold cursor-pointer">
+                    <Link href="/settings"><span className="mb-8 font-black border-b pb-4 md:float-right mt-2 align-middle text-black cursor-pointer hover:text-gray-400 z-10">
                     Settings
                 </span></Link>
-                    <Link href="/create"><span className="md:float-right mt-2 align-middle text-white ml- font-semibold cursor-pointer">
+                    <Link href="/create"><span className="mb-8 font-black border-b pb-4 md:float-right mt-2 align-middle text-black cursor-pointer hover:text-gray-400 z-10">
                     Create Post
                 </span></Link>
                 </div>}
                 </span>
                 </div>
             </div>
+
       </div>
         {/* {categories.map((category)=>(
             <Link key={category.slug} href={`/category/${category.slug}`}>
