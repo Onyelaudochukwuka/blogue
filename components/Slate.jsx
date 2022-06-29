a5d300830d2cc40fcfdd50a1f13f227b8af4b39c
udochukwuka onyela <udochukwukaonyela@gmail.com>
2022-06-29 08:32:38 +0100

    fixes

--- components/Slate.jsx
+++ components/Slate.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, useMemo } from 'react';
+import React, { useState, useRef, useEffect, useMemo, useCallback } from 'react';
 import { createEditor, Editor, Transforms } from "slate";
 import { Boldmark } from './Slate'
 import { Slate, withReact, Editable } from "slate-react";
@@ -11,43 +11,74 @@ const Slat = () => {
         children: [{ text: "We have some base content." }]
     }
     ]);
-    const onKeyDown = (e,change) => {
-        
-        switch (e.key) {
-            case '&': {
-                e.preventDefault();
-                editor.insertText('and');
-                return true;
-            }
-                
+    const renderElement = useCallback(props => {
+        switch (props.element.type) {
+            case 'code':
+                return <CodeElement {...props} />
+            default:
+                return <DefaultElement {...props} />
         }
-    }
-    const renderMark = (props) => {
-        switch (props.mark.type) {
-            case 'bold':
-                return <Boldmark {...props} />
-        }
-    }
-  return (
-      <div>
-          <Slate
-              editor={editor}
-              value={value}
-              renderMark={renderMark}
-              onChange={(newValue) => {
-                  console.log(newValue)
-                  return setValue(newValue)
+    }, [])
 
-              }}
-          >
-              <Editable
-                  
-                  onKeyDown={onKeyDown}
-                  style={{ border: "1px solid black" }}
-              
-              />
-          </Slate>
-      </div>  )
+    return (
+        <Slate editor={editor} value={value}
+            onChange={(newValue) => {
+                console.log(newValue);
+                setValue(newValue);
+        }}
+        >
+            <Editable
+                // Pass in the `renderElement` function.
+                renderElement={renderElement}
+                onKeyDown={(event) => {
+                    if (!event.ctrlKey) {
+                        return
+                    }
+
+                    switch (event.key) {
+                        // When "`" is pressed, keep our existing code block logic.
+                        case '/': {
+                            event.preventDefault()
+                            const [match] = Editor.nodes(editor, {
+                                match: n => n.type === 'code',
+                            })
+                            Transforms.setNodes(
+                                editor,
+                                { type: match ? 'paragraph' : 'code' },
+                                { match: n => Editor.isBlock(editor, n) }
+                            )
+                            break
+                        }
+
+                        // When "B" is pressed, bold the text in the selection.
+                        case 'b': {
+                            event.preventDefault()
+                            Transforms.setNodes(
+                                editor,
+                                { bold: true },
+                                // Apply it to text nodes, and split the text node up if the
+                                // selection is overlapping only part of it.
+                                { match: n => Text.isText(n), split: true }
+                            )
+                            break
+                        }
+                    }
+                }}
+            />
+        </Slate>
+    )
+}
+
+const CodeElement = props => {
+    return (
+        <pre {...props.attributes}>
+            <code>{props.children}</code>
+        </pre>
+    )
+}
+
+const DefaultElement = props => {
+    return <p {...props.attributes}>{props.children}</p>
 }
 
 export default Slat;
--- components/Slate/index.js
+++ components/Slate/index.js
@@ -1,1 +1,2 @@
 export {default as Boldmark } from './Boldmark'
+export { default as Default } from './Default'
